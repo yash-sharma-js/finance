2d597f6a1fdb8f2af040cceaa6f07dd9c6e2b180
Yash Sharma <yashsharma.js@gmail.com>
2026-04-02 10:08:07 +0530

    Initial commit

---
+++ src/main/java/com/github/finance_backend/FinanceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.github.finance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceBackendApplication.class, args);
+		
+	}
+
+}
